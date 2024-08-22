9e66085093686f8d49b651c9186e19f20a10dac6
Juliano Ambrósio <gestaojuliano99@gmail.com>
2024-08-21 22:59:27 -0300

    iniciando em aula a api

---
+++ src/main/java/com/juliano/Etapa1/service/ConsumoApi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.juliano.Etapa1.service;
+
+/**
+ *
+ * @author juliano
+ */
+public class ConsumoApi {
+    public String obterDados(String endereco) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+    
+}
---
+++ src/main/java/com/juliano/Etapa1/service/IConverteDados.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.juliano.Etapa1.service;
+
+/**
+ *
+ * @author juliano
+ */
+public class IConverteDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
